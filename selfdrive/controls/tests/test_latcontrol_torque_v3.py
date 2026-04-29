b28f1dcaf1821415b7004f7baf8019cd16ca74d4
jjolano <jjolano@me.com>
2026-04-29 08:36:15 -0400

    controls: add aggressive adaptive torque v3 controller

--- selfdrive/controls/tests/test_latcontrol_torque_v3.py
+++ selfdrive/controls/tests/test_latcontrol_torque_v3.py
@@ -21,3 +21,107 @@ def test_v3_adaptive_torque_schema_fields_exist():
   assert adaptive_log.modelMode == 2
   assert adaptive_log.authorityBand == 1
   assert adaptive_log.sampleAccepted
+
+
+import numpy as np
+import sys
+import types
+
+from cereal import car
+from opendbc.car.car_helpers import interfaces
+from opendbc.car.gm.values import CAR as GM
+from opendbc.car.toyota.values import CAR as TOYOTA
+from opendbc.car.vehicle_model import VehicleModel
+
+from openpilot.common.realtime import DT_CTRL
+from openpilot.selfdrive.car.helpers import convert_to_capnp
+from openpilot.selfdrive.locationd.helpers import Measurement, Pose
+from openpilot.sunnypilot.selfdrive.controls.lib.nnlc.helpers import MOCK_MODEL_PATH
+from openpilot.sunnypilot.selfdrive.controls.lib.torque_v3_authority import AuthorityBand
+from openpilot.sunnypilot.selfdrive.controls.lib.torque_v3_model import TorqueModelMode
+
+params_pyx = types.ModuleType("openpilot.common.params_pyx")
+
+
+class FakeParams:
+  def get_bool(self, _key: str) -> bool:
+    return False
+
+  def remove(self, _key: str) -> None:
+    pass
+
+  def get(self, _key: str, *_args, **_kwargs):
+    return None
+
+
+params_pyx.Params = FakeParams
+params_pyx.ParamKeyFlag = object
+params_pyx.ParamKeyType = object
+params_pyx.UnknownKeyName = RuntimeError
+sys.modules.setdefault("openpilot.common.params_pyx", params_pyx)
+
+from openpilot.sunnypilot.selfdrive.controls.lib.latcontrol_torque_v3 import LatControlTorque
+
+
+def get_controller(car_name, force_pid=False):
+  CarInterface = interfaces[car_name]
+  CP = CarInterface.get_non_essential_params(car_name)
+  CP_SP = CarInterface.get_non_essential_params_sp(CP, car_name)
+  CP_SP.neuralNetworkLateralControl.model.path = MOCK_MODEL_PATH
+  CI = CarInterface(CP, CP_SP)
+  if force_pid:
+    CP.lateralTuning.init('pid')
+    CP.lateralTuning.pid.kpBP = [0.0]
+    CP.lateralTuning.pid.kpV = [0.1]
+    CP.lateralTuning.pid.kiBP = [0.0]
+    CP.lateralTuning.pid.kiV = [0.01]
+  CP_SP = convert_to_capnp(CP_SP)
+  VM = VehicleModel(CP)
+  controller = LatControlTorque(CP.as_reader(), CP_SP.as_reader(), CI, DT_CTRL)
+  return controller, VM
+
+
+def make_pose():
+  zeros = np.zeros(3)
+  return Pose(Measurement(zeros, zeros), Measurement(zeros, zeros), Measurement(zeros, zeros), Measurement(zeros, zeros))
+
+
+def test_v3_native_torque_controller_logs_model_state():
+  controller, VM = get_controller(TOYOTA.TOYOTA_COROLLA_TSS2)
+  CS = car.CarState.new_message()
+  CS.vEgo = 20.0
+  params = log.LiveParametersData.new_message()
+
+  _, _, lac_log = controller.update(True, CS, VM, params, False, 5e-4, make_pose(), False, 0.2)
+
+  assert lac_log.version == 3
+  assert lac_log.adaptiveTorqueState.modelMode in (TorqueModelMode.native, TorqueModelMode.learned)
+  assert lac_log.adaptiveTorqueState.authorityScale > 0.0
+
+
+def test_v3_synthetic_pid_origin_starts_limited():
+  controller, VM = get_controller(TOYOTA.TOYOTA_COROLLA_TSS2, force_pid=True)
+  CS = car.CarState.new_message()
+  CS.vEgo = 20.0
+  params = log.LiveParametersData.new_message()
+
+  _, _, lac_log = controller.update(True, CS, VM, params, False, 5e-4, make_pose(), False, 0.2)
+
+  assert lac_log.version == 3
+  assert lac_log.adaptiveTorqueState.modelMode == TorqueModelMode.synthetic
+  assert lac_log.adaptiveTorqueState.authorityBand == AuthorityBand.limited
+  assert np.isclose(lac_log.adaptiveTorqueState.authorityScale, 0.45)
+
+
+def test_v3_smoke_on_gm_nonlinear_native_torque_platform():
+  controller, VM = get_controller(GM.CHEVROLET_BOLT_EUV)
+  CS = car.CarState.new_message()
+  CS.vEgo = 20.0
+  CS.steeringAngleDeg = 5.0
+  CS.steeringRateDeg = 25.0
+  params = log.LiveParametersData.new_message()
+
+  _, _, lac_log = controller.update(True, CS, VM, params, False, 0.001, make_pose(), False, 0.2)
+
+  assert lac_log.version == 3
+  assert np.isfinite(lac_log.output)
