38c68a429e634c4001579434e211f9ec8022dbe0
jjolano <jjolano@me.com>
2026-06-29 03:01:41 +0000

    custom/longitudinal: move speed-limit stack

--- sunnypilot/custom/longitudinal/speed_limit/__init__.py
+++ sunnypilot/custom/longitudinal/speed_limit/__init__.py
@@ -4,9 +4,3 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
-
-from openpilot.sunnypilot.custom.longitudinal.speed_limit.types import (
-  Policy as Policy,
-  OffsetType as OffsetType,
-  Mode as Mode,
-)
--- sunnypilot/selfdrive/controls/lib/speed_limit/__init__.py
+++ sunnypilot/selfdrive/controls/lib/speed_limit/__init__.py
@@ -4,16 +4,9 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
-LIMIT_ADAPT_ACC = -1.  # m/s^2 Ideal acceleration for the adapting (braking) phase when approaching speed limits.
-LIMIT_MAX_MAP_DATA_AGE = 10.  # s Maximum time to hold to map data, then consider it invalid inside limits controllers.
-
-# Speed Limit Assist constants
-PCM_LONG_REQUIRED_MAX_SET_SPEED = {
-  True: (33.3333, 36.1111),  # km/h, (120, 130)
-  False: (31.2928, 35.7632),  # mph, (70, 80)
-}
-
-CONFIRM_SPEED_THRESHOLD = {
-  True: 80,   # km/h
-  False: 50,  # mph
-}
+from openpilot.sunnypilot.custom.longitudinal.speed_limit.constants import (
+  LIMIT_ADAPT_ACC as LIMIT_ADAPT_ACC,
+  LIMIT_MAX_MAP_DATA_AGE as LIMIT_MAX_MAP_DATA_AGE,
+  PCM_LONG_REQUIRED_MAX_SET_SPEED as PCM_LONG_REQUIRED_MAX_SET_SPEED,
+  CONFIRM_SPEED_THRESHOLD as CONFIRM_SPEED_THRESHOLD,
+)
